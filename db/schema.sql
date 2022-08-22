e8d8c1cb905c306c74076874d37426e88601aa92
Jake McMillan <jakewmcmillan@gmail.com>
2022-08-22 17:03:01 -0400

    Adding menu functions

--- db/schema.sql
+++ db/schema.sql
@@ -13,9 +13,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -24,7 +22,5 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
+const { quiet } = require('nodemon/lib/utils');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,21 +20,123 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_db database.')
 );
 
+function menu () {
+    inquirer.prompt ([
+        {
+            name: 'menu',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employees',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit',
+            ]
+        }
+    ])
+    .then((answers) => {
+        const { choices } = answers;
 
+        if (choices === 'View All Employees') {
+            showEmployee();
+        }
+        if (choices === 'Add Employees') {
+            addEmployee();
+        }
+        if (choices === 'Update Employee Role') {
+            updateEmployeeRole();
+        }
+        if (choices === 'View All Roles') {
+            showRole();
+        }
+        if (choices === 'Add Role') {
+            addRole();
+        }
+        if (choices === 'View All Departments') {
+            showDepartment();
+        }
+        if (choices === 'Add Departments') {
+            addDepartment();
+        }
+        if (choices === 'Quit') {
+            quit();
+        };
+    });
+};
 
-// app.post('/api/new-employee', ({ body }, res) => {
-//     const sql = `INSERT into department (name)
-//         VALUES (?)`;
-//     const params = [body.name];
+function showDepartment() {
+    var query = 'SELECT * FROM department';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('Showing Departments:');
+        console.table('Departments:', res);
+        menu();
+    })
+}
 
-//     db.query(sql, params, (err, result) => {
-//         if (err) {
-//             res.status(400).json({ error: err.message });
-//             return;
-//         }
-//         res.json({
-//             message: 'success',
-//             data: body
-//         });
-//     });
-// });
+function showEmployee() {
+    var query = 'SELECT * FROM employee';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('Showing Employees:');
+        console.table('Employees:', res);
+        menu();
+    })
+};
+
+function addEmployee() {
+    var query = '';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('Adding employee:');
+        console.table('New Employee', res);
+        menu();
+    })
+};
+
+function updateEmployeeRole() {
+    var query = '';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('Updating Employee:');
+        console.table('Updating Employee');
+        menu();
+    })
+};
+
+function showRole() {
+    var query = 'SELECT * FROM role';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('Showing Role:');
+        console.table('Role');
+        menu();
+    })
+};
+
+function addRole() {
+    var query = 'SELECT * FROM role';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('Adding Role:');
+        console.table('Role');
+        menu();
+    })
+};
+
+function addDepartment() {
+    var query = 'SELECT * FROM department';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log('Adding Department:');
+        console.table('New Department');
+    })
+};
+
+function quit() {
+    process.exit();
+};

c5befe7a9cd9bd64eb21cf65a4d784612b40daa6
Jake McMillan <jakewmcmillan@gmail.com>
2022-08-22 14:48:21 -0400

    Installing table

--- server.js
+++ server.js
@@ -18,21 +18,21 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_db database.')
 );
 
-app.post('/api/new-employee', ({ body }, res) => {
-    const sql = `INSERT into department (name)
-        VALUES (?)`;
-    const params = [body.name];
 
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    });
-});
 
-app.get
+// app.post('/api/new-employee', ({ body }, res) => {
+//     const sql = `INSERT into department (name)
+//         VALUES (?)`;
+//     const params = [body.name];
+
+//     db.query(sql, params, (err, result) => {
+//         if (err) {
+//             res.status(400).json({ error: err.message });
+//             return;
+//         }
+//         res.json({
+//             message: 'success',
+//             data: body
+//         });
+//     });
+// });
